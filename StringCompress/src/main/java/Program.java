e958cad4ec7cde98068969706382841fae41e154
cubissimo13 <roman.kis@gmail.com>
2017-08-27 20:33:04 +0300

    start

--- StringCompress/src/main/java/Program.java
+++ StringCompress/src/main/java/Program.java
@@ -2,16 +2,26 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println(compressString("aaabbccccddee"));
-
     }
 
-    private static String compressString(String string){
-        StringBuilder result;
-        int i = 0;
-        while(i<= string.length()){
-
+    private static StringBuilder compressString(String string){
+        StringBuilder result = new StringBuilder();
+        char word = string.charAt(0);
+        int sum = 1;
+        int iterator = 1;
+        while(iterator < string.length()){
+            if (word == (string.charAt(iterator))){
+                sum++;
+            } else {
+                result.append(sum).append(word);
+                sum=1;
+                word = string.charAt(iterator);
+            }
+            iterator++;
+            if (iterator == string.length()){
+                result.append(sum).append(word);
+            }
         }
-
         return result;
     }
 }

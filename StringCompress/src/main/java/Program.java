dff3513648dabe3206f4625c53590853448d4812
cubissimo13 <roman.kis@gmail.com>
2017-08-27 14:23:25 +0300

    start

---
+++ StringCompress/src/main/java/Program.java
@@ -0,0 +1,17 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(compressString("aaabbccccddee"));
+
+    }
+
+    private static String compressString(String string){
+        StringBuilder result;
+        int i = 0;
+        while(i<= string.length()){
+
+        }
+
+        return result;
+    }
+}
